87e152b3583407e38184a154c469df81dfaf06bb
Chopper1337 <josephmg@tutanota.com>
2022-06-27 15:46:00 +0100

    Updated.

--- assets/js/store.js
+++ assets/js/store.js
@@ -4,10 +4,11 @@
 var loginStatus = "0";
 
 // Run the CheckLogin function upon loading this JS file
-CheckLogin();
+//CheckLogin();
+updateCart();
 
 // Log in user
-function Login(){
+function Login() {
     // Set loggedIn item in local storage to 1, to reflect login status
     localStorage.setItem("loggedIn", "1");
     // Log the action to console
@@ -23,7 +24,7 @@ function Login(){
 
 
 //Log out user
-function Logout(){
+function Logout() {
     // Set loggedIn item in local storage to 0, to reflect login status
     localStorage.setItem("loggedIn", "0");
     // Log the action to console
@@ -33,54 +34,109 @@ function Logout(){
 }
 
 //Check if the user is logged in
-function CheckLogin(){
+function CheckLogin() {
     //If they are logged in
-    if(localStorage.getItem("loggedIn") == 1){
+    if (localStorage.getItem("loggedIn") == 1) {
         // Log state to console
         console.log("User is logged in")
         // Set local variable to reflect status
         loginStatus = "1"
         // Change text in login/logout button to say logout
         document.getElementById("loginNavBTN").innerHTML = "Logout";
+        refreshCart()
+        return 1;
     }
-    else{
+    else {
         // Log state to console
         console.log("User is not logged in")
         // Set local variable to reflect status
         loginStatus = "0"
         // Change text in login/logout button to say login
         document.getElementById("loginNavBTN").innerHTML = "Login/Register";
+        document.getElementById("lblCartCount").innerHTML = "";
+        return 0;
+        refreshCart()
     }
+
 }
 
 //Handles functionality behind login/logout button
-function LoginLogoutBTN(){
+function LoginLogoutBTN() {
     console.log("Login/logout button clicked.")
-    if(loginStatus == 1){
+    if (loginStatus == 1) {
         Logout();
         console.log("LoginLogoutBTN pressed, user already logged in so they will be logged out")
     }
-    else{
+    else {
         var myModal = document.querySelector('#login')
         var modal = bootstrap.Modal.getOrCreateInstance(myModal) // Returns a Bootstrap modal instance
         modal.show()
     }
+    
 }
 
 
 //////// Currency changing logic ////////
 
 // Change the displayed currency as per selection
-function changeCurrency(currency){
+function changeCurrency(currency) {
+    let i = 0;
     console.log("Changing currency...")
-    if(currency === "euro"){
+    if (currency === "euro") {
         console.log("Changing currency to euro")
+        while (i < 12) {
+            document.getElementsByClassName("currency")[i].innerHTML = document.getElementsByClassName("currency")[i].innerHTML.replace(/^./g, '€');
+            i++;
+        }
     }
-    else if(currency === "usd"){
+    else if (currency === "usd") {
         console.log("Changing currency to usd")
+        while (i < 12) {
+            document.getElementsByClassName("currency")[i].innerHTML = document.getElementsByClassName("currency")[i].innerHTML.replace(/^./g, '$');
+            i++;
+        }
     }
-    else{
-        console.log("Changing currency to cad")
+    else {
+        console.log("Changing currency to GBP")
+        while (i < 12) {
+            document.getElementsByClassName("currency")[i].innerHTML = document.getElementsByClassName("currency")[i].innerHTML.replace(/^./g, '£');
+            i++;
+        }
     }
 
+}
+
+// Updates the cart
+function updateCart(addItem) {
+    // Read local storage for cart items
+    var currentCart = localStorage.getItem("cartItems")
+
+    // If the user is logged in
+    if (CheckLogin() == 1) {
+        // If we are supposed to add an item
+        if (addItem > 0) {
+            // If the current cart is not empty
+            if (currentCart > 0) {
+                // Add one to the carts value
+                localStorage.setItem("cartItems", parseInt(currentCart) + 1)
+            }
+            else {
+                // If it is empty (it doesn't exist), create it
+                localStorage.setItem("cartItems", "1");
+            }
+
+            currentCart = localStorage.getItem("cartItems")
+            document.getElementById("lblCartCount").innerHTML = currentCart
+        }
+    }
+    //If the user isn't logged in, hide the cart item counter text
+    else {
+        document.getElementById("lblCartCount").innerHTML = "";
+    }
+}
+
+function refreshCart() {
+    // Read local storage for cart items
+    var currentCart = localStorage.getItem("cartItems")
+    document.getElementById("lblCartCount").innerHTML = currentCart
 }
